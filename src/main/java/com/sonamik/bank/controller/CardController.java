909c205202c2a12761fcc31d416a2c65869334a5
Sona <sonmiq@gmail.com>
2022-05-27 10:33:10 +0400

    commit

--- src/main/java/com/sonamik/bank/controller/CardController.java
+++ src/main/java/com/sonamik/bank/controller/CardController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
+
 
 
 @RestController
